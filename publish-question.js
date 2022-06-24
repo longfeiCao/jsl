4739a1397f02c60b39cc8643f8fd3c296d920b83
LAPTOP-OPDN7CDI\86183 <1107084702@qq.com>
2022-06-24 19:08:30 +0800

    update

--- publish-question.js
+++ publish-question.js
@@ -85,6 +85,30 @@ function pubqingxiang(title,qid,pic) {
   });
 }
 
+/*function pinglunqingxiang(id) {
+  fetch(`https://jsjsl.lexiangla.com/api/v1/shares/${id}/replies`, {
+    "headers": {
+      "accept": "application/json, text/plain, *!/!*",
+      "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
+      "content-type": "application/json;charset=UTF-8",
+      "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"101\", \"Google Chrome\";v=\"101\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "\"Windows\"",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+      "x-auth-type": "api",
+      "x-requested-with": "XMLHttpRequest",
+      "x-xsrf-token": "a6MBea3rZtk%2BfpJ5I2MGGsYtdwVwMBoix%2F5DxrgEayK2EUkaSUC42Rue%2Bn0s7GXudpge32Pa7BBEzVQnezSD4D1wLZqyndnofqXCznUQKAU%3D",
+      "cookie": "company_server_type=workwechat; company_code=jsjsl; company_old_code=eb59bc2c463111ea97115254002f1020; company_display_name=%E6%B1%9F%E8%8B%8F%E9%87%91%E4%B8%9D%E5%88%A9; ti18nLng=zh-CN; token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvbGV4aWFuZ2xhLmNvbVwvYXV0aFwvd2VjaGF0X2xvZ2luX2NhbGxiYWNrIiwiaWF0IjoxNjU2MDUzNjYxLCJleHAiOjE2NTg2NDU2NjEsIm5iZiI6MTY1NjA1MzY2MSwianRpIjoicmxPNXBsZDVTcVdhazB2RCIsInN1YiI6IjU4MjEyNDMwNGYxMDExZWE4ZWU4NTI1NDAwZWRlZjIxIiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyIsImNvbXBhbnlfaWQiOiJlYjVhNzY3NjQ2MzExMWVhODc4YjUyNTQwMDJmMTAyMCIsInN0YWZmX3V1aWQiOiI1ODE2MDNiNjRmMTAxMWVhYjY1MDUyNTQwMGVkZWYyMSJ9.0dob-sUIM9qa82a5zGmbyc4DHCVuL1tu8NkkG2oJMW4; XSRF-TOKEN=a6MBea3rZtk%252BfpJ5I2MGGsYtdwVwMBoix%252F5DxrgEayK2EUkaSUC42Rue%252Bn0s7GXudpge32Pa7BBEzVQnezSD4D1wLZqyndnofqXCznUQKAU%253D",
+      "Referer": "https://jsjsl.lexiangla.com/company/global/moments?company_from=jsjsl",
+      "Referrer-Policy": "strict-origin-when-cross-origin"
+    },
+    "body": "{\"module_type\":\"company\",\"module_id\":\"global\",\"content\":\"再学习\"}",
+    "method": "POST"
+  });
+}*/
+
 
 function getWendangList() {
   fetch("https://jsjsl.lexiangla.com/api/v1/docs?filter=recommended&limit=20&page=1", {
@@ -116,6 +140,7 @@ function getWendangList() {
     /*pinglunwendang(list[3].target_id)*/
     dianzan(list[5].target_id)
     shoucang(list[5].target_id)
+    /*pinglunqingxiang(list[0].target_id)*/
   })
 }
 
