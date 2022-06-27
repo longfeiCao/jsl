5200d0fc59b10d5c713e97372b67b787b0c48a77
longfeiCao <1107084702@qq.com>
2022-06-27 09:48:38 +0800

    Update sign.sh

--- sign.sh
+++ sign.sh
@@ -1,20 +1,12 @@
 curl 'https://jsjsl.lexiangla.com/api/v1/points/check-in' \
   -X 'POST' \
-  -H 'authority: jsjsl.lexiangla.com' \
-  -H 'accept: application/json, text/plain, */*' \
-  -H 'accept-language: zh-CN,zh;q=0.9' \
-  -H 'content-length: 0' \
-  -H "cookie": "company_server_type=workwechat; company_code=jsjsl; company_old_code=eb59bc2c463111ea97115254002f1020; company_display_name=%E6%B1%9F%E8%8B%8F%E9%87%91%E4%B8%9D%E5%88%A9; ti18nLng=zh-CN; token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvbGV4aWFuZ2xhLmNvbVwvYXV0aFwvd2VjaGF0X2xvZ2luX2NhbGxiYWNrIiwiaWF0IjoxNjU2MDUzNjYxLCJleHAiOjE2NTg2NDU2NjEsIm5iZiI6MTY1NjA1MzY2MSwianRpIjoicmxPNXBsZDVTcVdhazB2RCIsInN1YiI6IjU4MjEyNDMwNGYxMDExZWE4ZWU4NTI1NDAwZWRlZjIxIiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyIsImNvbXBhbnlfaWQiOiJlYjVhNzY3NjQ2MzExMWVhODc4YjUyNTQwMDJmMTAyMCIsInN0YWZmX3V1aWQiOiI1ODE2MDNiNjRmMTAxMWVhYjY1MDUyNTQwMGVkZWYyMSJ9.0dob-sUIM9qa82a5zGmbyc4DHCVuL1tu8NkkG2oJMW4; XSRF-TOKEN=a6MBea3rZtk%252BfpJ5I2MGGsYtdwVwMBoix%252F5DxrgEayK2EUkaSUC42Rue%252Bn0s7GXuLSiD73wGHwmzJy%252FiRi%252FesKZKX0zprSW1AQVxxvWJSAI%253D" \
-  -H 'origin: https://jsjsl.lexiangla.com' \
-  -H 'referer: https://jsjsl.lexiangla.com/' \
   -H 'sec-ch-ua: " Not A;Brand";v="99", "Chromium";v="101", "Google Chrome";v="101"' \
+  -H 'X-XSRF-TOKEN: a6MBea3rZtk%2BfpJ5I2MGGsYtdwVwMBoix%2F5DxrgEayK2EUkaSUC42Rue%2Bn0s7GXuIR2P5nNFri5NzblWG%2FvWkHSyhP%2FVBZwLKT6is%2BF5Rsk%3D' \
+  -H 'X-Auth-Type: api' \
   -H 'sec-ch-ua-mobile: ?0' \
+  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36' \
+  -H 'Accept: application/json, text/plain, */*' \
+  -H 'Referer: https://jsjsl.lexiangla.com/notifications?company_from=jsjsl' \
+  -H 'X-Requested-With: XMLHttpRequest' \
   -H 'sec-ch-ua-platform: "Windows"' \
-  -H 'sec-fetch-dest: empty' \
-  -H 'sec-fetch-mode: cors' \
-  -H 'sec-fetch-site: same-origin' \
-  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36' \
-  -H 'x-auth-type: api' \
-  -H 'x-requested-with: XMLHttpRequest' \
-  -H 'x-xsrf-token: a6MBea3rZtk%2BfpJ5I2MGGsYtdwVwMBoix%2F5DxrgEayK2EUkaSUC42Rue%2Bn0s7GXuLSiD73wGHwmzJy%2FiRi%2FesKZKX0zprSW1AQVxxvWJSAI%3D' \
   --compressed
