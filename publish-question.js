4c84c96377e4793836629df540a3594a734e9975
longfeiCao <1107084702@qq.com>
2022-09-23 11:12:54 +0800

    Create publish-question.js

--- publish-question.js
+++ publish-question.js
@@ -1,29 +1,22 @@
 function sign(){
   fetch("https://jsjsl.lexiangla.com/api/v1/points/check-in", {
-    "headers": {
-      "accept": "application/json, text/plain, */*",
-      "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
-      "cache-control": "no-cache",
-      "pragma": "no-cache",
-      "sec-ch-ua": "\".Not/A)Brand\";v=\"99\", \"Google Chrome\";v=\"103\", \"Chromium\";v=\"103\"",
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": "\"Windows\"",
-      "sec-fetch-dest": "empty",
-      "sec-fetch-mode": "cors",
-      "sec-fetch-site": "same-origin",
-      "x-auth-type": "api",
-      "x-requested-with": "XMLHttpRequest",
-      "x-xsrf-token": "a6MBea3rZtk%2BfpJ5I2MGGsYtdwVwMBoix%2F5DxrgEayK2EUkaSUC42Rue%2Bn0s7GXuVvPiVy5j5zAYKN%2FZR2t5YuD7RH9ziC5MruTY72%2FY0XM%3D",
-      "cookie": "company_server_type=workwechat; company_code=jsjsl; company_old_code=eb59bc2c463111ea97115254002f1020; company_display_name=%E6%B1%9F%E8%8B%8F%E9%87%91%E4%B8%9D%E5%88%A9; token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvbGV4aWFuZ2xhLmNvbVwvYXV0aFwvd2VjaGF0X2xvZ2luX2NhbGxiYWNrIiwiaWF0IjoxNjU4ODI1NjEyLCJleHAiOjE2NjE0MTc2MTIsIm5iZiI6MTY1ODgyNTYxMiwianRpIjoieDJ3cEZPOENBaHk3TDVJQiIsInN1YiI6IjU4MjEyNDMwNGYxMDExZWE4ZWU4NTI1NDAwZWRlZjIxIiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyIsImNvbXBhbnlfaWQiOiJlYjVhNzY3NjQ2MzExMWVhODc4YjUyNTQwMDJmMTAyMCIsInN0YWZmX3V1aWQiOiI1ODE2MDNiNjRmMTAxMWVhYjY1MDUyNTQwMGVkZWYyMSJ9.od2ydnzXiF9xGv1pSo9V1-TcgeyuKiaUbfar7lmO4as; ti18nLng=zh-CN; XSRF-TOKEN=a6MBea3rZtk%252BfpJ5I2MGGsYtdwVwMBoix%252F5DxrgEayK2EUkaSUC42Rue%252Bn0s7GXuVvPiVy5j5zAYKN%252FZR2t5YuD7RH9ziC5MruTY72%252FY0XM%253D",
-      "Referer": "https://jsjsl.lexiangla.com/staff/points",
-      "Referrer-Policy": "strict-origin-when-cross-origin"
-    },
-    "body": null,
-    "method": "POST"
-  }).then(data=>{
-    console.log(JSON.stringify(data))
-  })
+  "headers": {
+    "accept": "application/json, text/plain, */*",
+    "sec-ch-ua": "\".Not/A)Brand\";v=\"99\", \"Google Chrome\";v=\"103\", \"Chromium\";v=\"103\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "x-auth-type": "api",
+    "x-requested-with": "XMLHttpRequest",
+    "x-xsrf-token": "a6MBea3rZtk%2BfpJ5I2MGGsYtdwVwMBoix%2F5DxrgEayK2EUkaSUC42Rue%2Bn0s7GXukqUl781jmZS8K57iyi1kMDC7bRSv7ITpyCUZUpq4IWU%3D",
+    "Referer": "https://jsjsl.lexiangla.com/notifications?company_from=jsjsl",
+    "Referrer-Policy": "strict-origin-when-cross-origin"
+  },
+  "body": null,
+  "method": "POST"
+}).then(res=>{console.log(res)})
 }
+
+
 
 function parseRes(rb){
   // fetch('https://www.example.org')
