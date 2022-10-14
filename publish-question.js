30643ccacd4b2cd3160de76e4212a710ad418fce
longfeiCao <1107084702@qq.com>
2022-10-14 11:30:48 +0800

    Update publish-question.js

--- publish-question.js
+++ publish-question.js
@@ -2,17 +2,24 @@ function sign(){
   fetch("https://jsjsl.lexiangla.com/api/v1/points/check-in", {
   "headers": {
     "accept": "application/json, text/plain, */*",
+    "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
     "sec-ch-ua": "\".Not/A)Brand\";v=\"99\", \"Google Chrome\";v=\"103\", \"Chromium\";v=\"103\"",
     "sec-ch-ua-mobile": "?0",
     "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
     "x-auth-type": "api",
     "x-requested-with": "XMLHttpRequest",
-    "x-xsrf-token": "a6MBea3rZtk%2BfpJ5I2MGGsYtdwVwMBoix%2F5DxrgEayK2EUkaSUC42Rue%2Bn0s7GXukqUl781jmZS8K57iyi1kMDC7bRSv7ITpyCUZUpq4IWU%3D",
-    "Referer": "https://jsjsl.lexiangla.com/notifications?company_from=jsjsl",
+    "x-xsrf-token": "a6MBea3rZtk%2BfpJ5I2MGGsYtdwVwMBoix%2F5DxrgEayK2EUkaSUC42Rue%2Bn0s7GXuUXVHYGBC9Qix%2Bh4xGmCQqeL9m9zPCPX5gUlPTSrLMYw%3D",
+    "cookie": "token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvbGV4aWFuZ2xhLmNvbVwvYXV0aFwvd2VjaGF0X2xvZ2luX2NhbGxiYWNrIiwiaWF0IjoxNjY1NzE3NjY4LCJleHAiOjE2NjgzMDk2NjgsIm5iZiI6MTY2NTcxNzY2OCwianRpIjoiUGdCaXgwNVpCN2VaVTVLSyIsInN1YiI6IjU4MjEyNDMwNGYxMDExZWE4ZWU4NTI1NDAwZWRlZjIxIiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyIsImNvbXBhbnlfaWQiOiJlYjVhNzY3NjQ2MzExMWVhODc4YjUyNTQwMDJmMTAyMCIsInN0YWZmX3V1aWQiOiI1ODE2MDNiNjRmMTAxMWVhYjY1MDUyNTQwMGVkZWYyMSJ9.UHYUS1SWmkYH1w6LKydKEd3kwUTMdNgHAneXr5Ad36I; company_server_type=workwechat; company_code=jsjsl; company_old_code=eb59bc2c463111ea97115254002f1020; company_display_name=%E6%B1%9F%E8%8B%8F%E9%87%91%E4%B8%9D%E5%88%A9; ti18nLng=zh-CN; XSRF-TOKEN=a6MBea3rZtk%252BfpJ5I2MGGsYtdwVwMBoix%252F5DxrgEayK2EUkaSUC42Rue%252Bn0s7GXuUXVHYGBC9Qix%252Bh4xGmCQqeL9m9zPCPX5gUlPTSrLMYw%253D",
+    "Referer": "https://jsjsl.lexiangla.com/?company_from=jsjsl",
     "Referrer-Policy": "strict-origin-when-cross-origin"
   },
+  "body": null,
   "method": "POST"
-}).then(res=>{console.log(res)})
+}).then(response => response.json()).then(data => console.log(data)).catch(err => console.error(err));
+
 }
 
 
